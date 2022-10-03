4bec97e52386b011b657acd200b609a8e7e3b85a
Jakub Nawacki <jakubnawacki@gmail.com>
2022-10-03 19:57:24 +0200

    Updates

---
+++ precache-manifest.82e9b9a2e9132878bd9f271cb754ec35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be88dfb3b53bafcc70fe",
+    "url": "/weather-app-react/static/css/main.419fd79c.chunk.css"
+  },
+  {
+    "revision": "be88dfb3b53bafcc70fe",
+    "url": "/weather-app-react/static/js/main.be88dfb3.chunk.js"
+  },
+  {
+    "revision": "ba6146680d69b685b97c",
+    "url": "/weather-app-react/static/js/1.ba614668.chunk.js"
+  },
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "06d38e4bcd1dd6a242246cfce004f74e",
+    "url": "/weather-app-react/static/media/background.06d38e4b.jpg"
+  },
+  {
+    "revision": "90c6bd2fe6ca0e76f161bb3a8f9d081f",
+    "url": "/weather-app-react/index.html"
+  }
+];
